8f5cffd6165fdfc37beffac6c7037bae9e88f470
p.horovenko <p.horovenko@LINKER-W3>
2022-04-19 13:48:26 +0300

    rebase

---
+++ Encryptions/Keys/IKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheoryOfInformation.lab1.Encryptions.Keys
+{
+    public interface IKey
+    {
+        IEnumerable<long> Encrypte(byte[] file, out byte resize);
+        byte[] Dencrypte(byte[] file);
+    }
+}
